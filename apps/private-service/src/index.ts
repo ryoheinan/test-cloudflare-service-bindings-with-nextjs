0160ab1f55f7e2f4179820a37a2bcff7cb03b896
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2023-10-09 23:15:31 +0900

    fix: Fix return value of private-service

--- apps/private-service/src/index.ts
+++ apps/private-service/src/index.ts
@@ -1,19 +1,19 @@
-import { Hono } from "hono";
+import { Hono } from 'hono'
 
 type Bindings = {
-  INTERNAL_TOKEN: string;
-};
+  INTERNAL_TOKEN: string
+}
 
-const app = new Hono<{ Bindings: Bindings }>().basePath("/api/private");
+const app = new Hono<{ Bindings: Bindings }>().basePath('/api/private')
 
-app.use("*", async (c, next) => {
-  const token = c.req.raw.headers.get("x-custom-token");
+app.use('*', async (c, next) => {
+  const token = c.req.raw.headers.get('x-custom-token')
   if (token !== c.env.INTERNAL_TOKEN) {
-    return c.text("Unauthorized", 401);
+    return c.json({ msg: 'Unauthorized' }, 401)
   }
-  await next();
-});
+  await next()
+})
 
-app.get("/", (c) => c.text("Hello Private Service!"));
+app.get('/', (c) => c.json({ msg: 'Hello Private Service!' }))
 
-export default app;
+export default app
